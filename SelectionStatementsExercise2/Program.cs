c61e0a342ce259cb35e002820684bdb79e59a40a
Monica Washington <monica.lynnwashington@gmail.com>
2026-02-20 21:16:51 -0700

    Completed Selection Statment Exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          FavSubject();  
+        }
+
+        public static void FavSubject()
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            string favoriteSubject = Console.ReadLine();
+
+            switch (favoriteSubject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is not my favorite math.");
+                    break;
+                case "science":
+                    Console.WriteLine("I enjoyed science, especially technology.");
+                    break;
+                case "physics":
+                    Console.WriteLine("Physics is the absolute best.");
+                    break;
+                case "history":
+                    Console.WriteLine("I enjoy history as an adult, but really didn't like it in school.");
+                    break;
+                case "geography":
+                    Console.WriteLine("I enjoy learing about the world.");
+                    break;
+                default:
+                    Console.WriteLine("Interesting choice. What do you like about it?");
+                    break;
+                
+            }
         }
     }
 }
